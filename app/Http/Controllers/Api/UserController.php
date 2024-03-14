bfe3927860d684b897daf49b40debb7e39164d61
kkamara <kamaracomputers@gmail.com>
2024-03-14 22:16:45 +0000

    Add Api/UserResource

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Response;
 use App\Models\User;
 use App\Http\Resources\Web\UserResource;
+use App\Http\Resources\Api\UserResource as APIUserResource;
 
 class UserController extends Controller
 {
